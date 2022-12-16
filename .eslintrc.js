7bec1aaac2e84e48f4fe2881280e2a27e1bd1399
brandoncfallin <brandonfallin@gmail.com>
2022-12-16 16:49:09 -0500

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,7 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    amd: true,
-    node: true,
-    es6: true,
-  },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
+  extends: '@react-native-community',
   rules: {
-    'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 0,
-    'no-unused-vars': 0,
-    'react/no-unescaped-entities': 0,
+    'prettier/prettier': 0,
   },
-}
+};
