3f7ca13554982bd5379b9ec676adc5dccfc66fff
brandoncfallin <brandonfallin@gmail.com>
2022-12-16 17:11:42 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,238 @@
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import formatDate from '@/lib/utils/formatDate'
+import Image from 'next/image'
+
+import NewsletterForm from '@/components/NewsletterForm'
+
+const MAX_DISPLAY = 5
+
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter('blog')
+
+  return { props: { posts } }
+}
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-4 md:space-y-5">
+          <div className="relative mb-8 mr-auto w-[100px] sm:mb-0 sm:w-[200px]">
+            <Image
+              src="/static/images/fallin.jpg"
+              alt="Portrait of Brandon Fallin."
+              width="120px"
+              height="120px"
+              className="rounded-full"
+            />
+          </div>
+
+          <div className="flex flex-col pr-8">
+            <h1 className="mb-1 text-3xl font-bold tracking-tight text-zinc-800 dark:text-white md:text-4xl">
+              Brandon Fallin
+            </h1>
+            <p className="mt-2 text-lg tracking-tight text-zinc-400 dark:text-zinc-450 md:text-lg">
+              Graduate Research Assistant @ University of Florida
+            </p>
+
+            <p className="my-4 leading-loose">
+              I’m currently living in Gainesville, FL and working at the{" "}
+              <a
+                className="focusable rounded-sm font-medium text-zinc-800 underline decoration-orange-500 decoration-2 underline-offset-2 transition duration-100 hover:text-orange-500 hover:decoration-orange-500/30 focus:text-orange-500 focus:ring-orange-500/40 dark:text-white dark:decoration-orange-400 dark:hover:text-orange-400 dark:hover:decoration-orange-400/30 dark:focus:text-orange-400 dark:focus:ring-orange-400/40"
+                href="http://corelab.mae.ufl.edu/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                Control, Optimization, and Robotics Engineering (CORE) Lab
+              </a>
+              {" "}while pursuing my PhD. My research focuses on differential privacy and its applications to control systems. I recently graduated from the{" "}
+              <a
+                className="focusable rounded-sm font-medium text-zinc-800 underline decoration-blue-500 decoration-2 underline-offset-2 transition duration-100 hover:text-blue-500 hover:decoration-sky-500/30 focus:text-blue-500 focus:ring-sky-500/40 dark:text-white dark:decoration-sky-400 dark:hover:text-sky-400 dark:hover:decoration-sky-400/30 dark:focus:text-sky-400 dark:focus:ring-sky-400/40"
+                href="https://www.excelia-group.com/"
+                rel="noreferrer"
+                target="_blank"
+              >
+                University of Florida
+              </a>
+              {" "}with Bachelors degrees in Mechanical and Aerospace Engineering. I'm passionate about photography, fitness, and all things tech.
+            </p>
+
+            <div className="mt-8 mb-8 flex flex-wrap gap-4 text-center">
+              <a
+                className="focusable flex flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-blue-500 py-2 px-2.5 font-medium text-white shadow-lg shadow-blue-500/10 transition selection:bg-white/30 hover:bg-blue-500/80 hover:shadow-blue-500/5 focus:ring-blue-500/40 dark:bg-blue-400 dark:text-zinc-900 dark:shadow-blue-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-blue-400/80 dark:hover:shadow-blue-400/5 dark:focus:ring-blue-400/40 sm:w-auto sm:px-3 sm:pl-2.5"
+                href="https://www.linkedin.com/in/brandonfallin"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <svg height="24" role="presentation" width="24" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    clipRule="evenodd"
+                    d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                  />
+                </svg>
+                <span className="hidden sm:inline">LinkedIn</span>
+              </a>
+              <a
+                className="focusable flex flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-gray-500 py-2 px-2.5 font-medium text-white shadow-lg shadow-gray-500/10 transition selection:bg-white/30 hover:bg-gray-500/80 hover:shadow-gray-500/5 focus:ring-gray-500/40 dark:bg-gray-200 dark:text-zinc-900 dark:shadow-gray-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-gray-400/80 dark:hover:shadow-gray-400/5 dark:focus:ring-gray-400/40 sm:w-auto sm:px-3 sm:pl-2.5"
+                href="https://github.com/brandoncfallin"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <svg height="24" role="presentation" width="24" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    clipRule="evenodd"
+                    d="M12 2C6.475 2 2 6.47 2 11.988c0 4.42 2.862 8.153 6.838 9.476.5.087.687-.212.687-.474 0-.238-.013-1.024-.013-1.86C7 19.59 6.35 18.517 6.15 17.955c-.113-.287-.6-1.174-1.025-1.411-.35-.187-.85-.65-.013-.662.788-.012 1.35.724 1.538 1.024.9 1.51 2.338 1.086 2.912.824.088-.65.35-1.086.638-1.336-2.225-.25-4.55-1.111-4.55-4.931 0-1.087.387-1.986 1.025-2.685-.1-.25-.45-1.273.1-2.646 0 0 .837-.263 2.75 1.023a9.29 9.29 0 0 1 2.5-.337c.85 0 1.7.113 2.5.337 1.912-1.298 2.75-1.023 2.75-1.023.55 1.373.2 2.397.1 2.646.637.7 1.025 1.586 1.025 2.685 0 3.832-2.337 4.681-4.562 4.931.362.312.675.912.675 1.848 0 1.336-.013 2.41-.013 2.747 0 .262.188.574.688.474C19.137 20.141 22 16.395 22 11.988 22 6.47 17.525 2 12 2Z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                  />
+                </svg>
+                <span className="hidden sm:inline">GitHub</span>
+              </a>
+              <a
+                className="focusable flex flex-none cursor-pointer items-center justify-center gap-2 rounded-md bg-orange-500 py-2 px-2.5 font-medium text-white shadow-lg shadow-orange-500/10 transition selection:bg-white/30 hover:bg-orange-500/80 hover:shadow-orange-500/5 focus:ring-orange-500/40 dark:bg-orange-400 dark:text-zinc-900 dark:shadow-orange-400/10 dark:selection:bg-zinc-900/30 dark:hover:bg-orange-400/80 dark:hover:shadow-orange-400/5 dark:focus:ring-orange-400/40 sm:w-auto sm:px-3 sm:pl-2.5"
+                href="mailto:brandonfallin@gmail.com"
+              >
+                <svg height="24" role="presentation" width="24" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    clipRule="evenodd"
+                    d="M7.727 2.959A10 10 0 0 1 22 11.999v.9a3.7 3.7 0 0 1-6.642 2.244 4.6 4.6 0 1 1-.741-6.928A1 1 0 0 1 16.6 8.4v4.5a1.7 1.7 0 1 0 3.4 0V12a8 8 0 1 0-3.136 6.353 1 1 0 1 1 1.216 1.587A10 10 0 1 1 7.727 2.96Zm6.873 9.04a2.6 2.6 0 1 0-5.2 0 2.6 2.6 0 0 0 5.2 0Z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                  />
+                </svg>
+                <span className="hidden sm:inline">Email</span>
+              </a>
+
+            </div>
+
+          </div>
+
+          <div className="content mt-12 sm:mt-24">
+            <h2 className="mb-2 text-3xl font-bold text-zinc-800 dark:text-white">Work Experience</h2>
+            <ul className="mt-8">
+              <li className="dark:text-zinc-350 my-5 flex items-center gap-4 text-zinc-500">
+                <a
+                  className="link focusable font-medium text-zinc-800 dark:text-white"
+                  href="https://ufl.edu/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <Image
+                    src="/static/images/workxp/uflogo.png"
+                    alt="UF"
+                    width="50px"
+                    height="50px"
+                    className="rounded-md bg-orange-500/80"
+                  />
+                </a>
+                <div className="flex min-w-0 flex-col justify-center">
+                  <p className="mb-1 flex items-center">
+                    <span className="truncate font-semibold text-zinc-700 dark:text-zinc-100">
+                      University of Florida
+                    </span>
+                    <span className="ml-1.5 inline-block flex-none translate-y-px rounded bg-zinc-100 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+                      Aug 2022
+                      <span className="text-zinc-350 dark:text-zinc-550 mx-0.5">—</span>
+                      Present
+                    </span>
+                  </p>
+                  <p className="flex items-center truncate">
+                    <span className="flex-1 truncate text-zinc-700 dark:text-zinc-100">
+                      Graduate Research Assistant
+                    </span>
+                  </p>
+                </div>
+
+              </li>
+              <p className="mt-2 text-lg tracking-tight text-zinc-400 dark:text-zinc-450 md:text-lg">
+                Pursuing my PhD in Aerospace Engineering under the advisement of Dr. Matthew Hale. Expected graduation May 2027
+              </p>
+              <li className="dark:text-zinc-350 my-5 flex items-center gap-4 text-zinc-500">
+                <a
+                  className="link focusable font-medium text-zinc-800 dark:text-white"
+                  href="https://upinorbit.io/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <Image
+                    src="/static/images/workxp/uflogo.png"
+                    alt="Orbit"
+                    width="50px"
+                    height="50px"
+                    className="rounded-md bg-purple-500/80"
+                  />
+                </a>
+                <div className="flex min-w-0 flex-col justify-center">
+                  <p className="mb-1 flex items-center">
+                    <span className="truncate font-semibold text-zinc-700 dark:text-zinc-100">
+                      Orbit
+                    </span>
+                    <span className="ml-1.5 inline-block flex-none translate-y-px rounded bg-zinc-100 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+                      Mar 2021
+                      <span className="text-zinc-350 dark:text-zinc-550 mx-0.5">—</span>
+                      Aug 2022
+                    </span>
+                  </p>
+                  <p className="flex items-center truncate">
+                    <span className="flex-1 truncate text-zinc-700 dark:text-zinc-100">
+                      Co-Founder & Chief Marketing Officer
+                    </span>
+                  </p>
+                </div>
+
+              </li>
+              <p className="mt-2 text-lg tracking-tight text-zinc-400 dark:text-zinc-450 md:text-lg">
+                Closed a M&A deal with Creatd, Inc. (Nasdaq CM: CRTD). Generated visual identity, web, and social media content in order to execute marketing and advertisement initiatives.
+              </p>
+              <li className="dark:text-zinc-350 my-5 flex items-center gap-4 text-zinc-500">
+                <a
+                  className="link focusable font-medium text-zinc-800 dark:text-white"
+                  href="https://www.powerdesigninc.us/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <Image
+                    src="/static/images/workxp/uflogo.png"
+                    alt="PDI"
+                    width="50px"
+                    height="50px"
+                    className="rounded-md bg-red-500/80"
+                  />
+                </a>
+                <div className="flex min-w-0 flex-col justify-center">
+                  <p className="mb-1 flex items-center">
+                    <span className="truncate font-semibold text-zinc-700 dark:text-zinc-100">
+                      Power Design, Inc.
+                    </span>
+                    <span className="ml-1.5 inline-block flex-none translate-y-px rounded bg-zinc-100 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+                      Jun 2021
+                      <span className="text-zinc-350 dark:text-zinc-550 mx-0.5">—</span>
+                      Aug 2021
+                    </span>
+                  </p>
+                  <p className="flex items-center truncate">
+                    <span className="flex-1 truncate text-zinc-700 dark:text-zinc-100">
+                      Mechanical Engineering Intern
+                    </span>
+                  </p>
+                </div>
+
+              </li>
+              <p className="mt-2 text-lg tracking-tight text-zinc-400 dark:text-zinc-450 md:text-lg">
+                Worked alongside Plumbing Engineers and Associate Plumbing Designers to produce technical drawings in Revit for multi-family high rise, retail, and amenity spaces. 
+              </p>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
