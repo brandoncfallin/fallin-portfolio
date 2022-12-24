0e01749466719f8e3e2ad33915caedc61367ab83
trophyguarder <brandonfallin@gmail.com>
2022-12-24 10:10:11 -0500

    nav update

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,10 +1,10 @@
 const headerNavLinks = [
-  { href: '/', title: 'home' },
-  { href: '/blog', title: 'notes' },
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Notes' },
   //{ href: '/tags', title: 'Tags' },
   //{ href: '/projects', title: 'Projects' },
   //{ href: '/about', title: 'about' },
-  { href: '/static/pdfs/resume.pdf', title: 'resume' },
+  { href: '/static/pdfs/resume.pdf', title: 'Resume' },
 ]
 
 export default headerNavLinks
